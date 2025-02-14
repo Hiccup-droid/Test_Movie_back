9041f8a652995462ef8de1d3f2c85218b0844027
gimbal-com <senseofpriderenovation@gmail.com>
2025-02-14 08:45:13 -0800

    add: claim list read and add api

--- controllers/claim.js
+++ controllers/claim.js
@@ -1,7 +1,40 @@
+import Claim from '../models/claim.js';
+
 export const getClaimRequestListByClient = (req, res) => {
-    
+    const clientId = req.query.clientId;
+
+    Claim
+    .find({c_job_owner: clientId})
+    .populate([
+        {path: 'c_claimer'},
+        {path: 'c_job'}
+    ])
+    .then(claims => {
+        return res.status(200).json({success: true, claims});
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({success: false, claims: []});
+    })
 }
 
 export const makeNewClaimRequestByPilot = (req, res) => {
-    
+    const {c_job_owner, c_job, c_content, c_claimer } = req.body;
+
+    const newClaim = new Claim({
+        c_job_owner,
+        c_job,
+        c_content,
+        c_claimer
+    });
+
+    newClaim
+    .save()
+    .then(() => {
+        return res.status(200).send({message: 'Claim succeed.', success: true});
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).send({message: 'Claim failed.', success: false})
+    })
 }
