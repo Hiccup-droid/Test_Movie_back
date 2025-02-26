45d23205b86db91ab4cc71106a5ac13fec9aa281
Hiccup <christopherfantory@gmail.com>
2025-02-26 15:28:15 -0500

    hiccup initialize backend project

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';  
-import User from '../models/user.js'; 
-import passport from 'passport';  
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import User from '../models/user.js';
+import passport from 'passport';
 
 // Options for JWT authentication
 const opts = {
@@ -10,22 +10,22 @@ const opts = {
 
 // Configuring passport to use the JwtStrategy
 passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {    
+    new JwtStrategy(opts, (jwt_payload, done) => {
         console.log(jwt_payload);
-                  // JwtStrategy validates the JWT and extracts the payload
+        // JwtStrategy validates the JWT and extracts the payload
         // Query the database to find a user with the ID from the JWT payload
         User.findById(jwt_payload._id)
-        .then((user) => {
-            if (user) {
-                return done(null, user);                        // If the user is found, pass the user to done()
-            } else {
-                return done(null, false);                       // If no user is found, pass false to done()
-            }
-        })
-        .catch(err => {
-            return done(err, false)
-        });
-    })      
+            .then((user) => {
+                if (user) {
+                    return done(null, user);                        // If the user is found, pass the user to done()
+                } else {
+                    return done(null, false);                       // If no user is found, pass false to done()
+                }
+            })
+            .catch(err => {
+                return done(err, false)
+            });
+    })
 );
 
 export default passport;
